63b2bd5de85a3721de3a505f92f4d51d40d0e9a9
travissong <1004404943@qq.com>
2022-12-06 23:48:31 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from src.route.user import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+#添加其它文件到路由
+add_user_fun(app) #1111
+
+#test commit
+if __name__ == '__main__':
+    app.run()

0825812398ce83786f92e93ca7d1ffe69d6bcbaa
travis2016 <312344899@qq.com>
2023-01-02 12:39:13 +0800

    update 20230102

--- src/logic/user_logic.py
+++ src/logic/user_logic.py
@@ -7,4 +7,7 @@ class user_class:
     def user_login(self):
         if request.method == 'POST':
             post_data = request.get_data()
+        else:
+            username = request.args.get("username")
+            pwd = request.args.get("password")
         return "{\"status\":1}"
